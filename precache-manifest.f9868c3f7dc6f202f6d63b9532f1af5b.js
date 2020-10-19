90dad9221f0e7d0ae6ac21644915617fcf90ff01
Drgit config --global user.email youremail@yourdomain.comiviz <kumar.driviz@gmail.com>
2020-10-19 11:17:46 +0530

    Updates

---
+++ precache-manifest.f9868c3f7dc6f202f6d63b9532f1af5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb2eb02e986258b688399e6a5ed8a82",
+    "url": "/TaskList-react/index.html"
+  },
+  {
+    "revision": "7df4bc0d9298f07abef1",
+    "url": "/TaskList-react/static/css/main.e75a9df0.chunk.css"
+  },
+  {
+    "revision": "bd0b89ee34b72e7ee0d0",
+    "url": "/TaskList-react/static/js/2.307cfd83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaskList-react/static/js/2.307cfd83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df4bc0d9298f07abef1",
+    "url": "/TaskList-react/static/js/main.9f74632c.chunk.js"
+  },
+  {
+    "revision": "e9aae8da65358ca2188f",
+    "url": "/TaskList-react/static/js/runtime-main.86b5a11f.js"
+  }
+]);
